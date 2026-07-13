7ffda58f9920b4f16e19be9326db527a152b91f6
Mateusz Maćkowski <m4tx@m4tx.pl>
2026-07-13 21:32:56 +0200

    chore: actually add v0.7 (#99)

--- cot-site-common/src/lib.rs
+++ cot-site-common/src/lib.rs
@@ -3,10 +3,11 @@ mod utils;
 pub use utils::{Version, VersionError};
 
 pub const MASTER_VERSION: &str = "master";
-pub const LATEST_VERSION: &str = "v0.6";
+pub const LATEST_VERSION: &str = "v0.7";
 pub const ALL_VERSIONS: &[&str] = &[
     MASTER_VERSION,
     LATEST_VERSION,
+    "v0.6",
     "v0.5",
     "v0.4",
     "v0.3",
